d68a0ed0013fd5ee0b23c6d32c34fe801846872a
Dāvis Mālnieks <daavis.90@gmail.com>
2012-08-28 19:48:33 +0300

    initial commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,132 @@
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# BoardConfigCommon.mk
+#
+# Product-common compile-time definitions.
+#
+
+LOCAL_PATH:= $(call my-dir)
+
+TARGET_GLOBAL_CFLAGS += -mfpu=vfp -mfloat-abi=softfp -Os
+TARGET_GLOBAL_CPPFLAGS += -mfpu=vfp -mfloat-abi=softfp -Os
+
+## Kernel
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+BOARD_KERNEL_BASE := 0x13600000
+BOARD_KERNEL_PAGESIZE := 4096
+TARGET_KERNEL_SOURCE := kernel/samsung/msm7x27
+
+## Platform
+TARGET_BOARD_PLATFORM := msm7x27
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/msm7x27-common/include
+
+## Webkit, browser
+JS_ENGINE := v8
+HTTP := chrome
+ENABLE_WEBGL := true
+TARGET_WEBKIT_USE_MORE_MEMORY := true
+
+## JIT
+WITH_JIT := true
+ENABLE_JSC_JIT := true
+
+## Camera
+USE_CAMERA_STUB := false
+BOARD_USE_FROYO_LIBCAMERA := true
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_HARDWARE
+
+## Qualcomm, display
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_NO_HW_VSYNC := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+USE_OPENGL_RENDERER := true
+TARGET_USES_GENLOCK := true
+BOARD_EGL_CFG := device/samsung/msm7x27-common/prebuilt/lib/egl/egl.cfg
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DREFRESH_RATE=60
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DBINDER_COMPAT
+COMMON_GLOBAL_CFLAGS += -DFORCE_CPU_UPLOAD -DQCOM_ICS_COMPAT
+
+## GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x27
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 1240
+
+## Other Qualcomm config
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+
+## Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_SAMSUNG_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+## FM
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := brcm2049
+
+## Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER := AR6000
+WPA_SUPPLICANT_VERSION := VER_0_6_X
+BOARD_WLAN_DEVICE := wlan0
+WIFI_DRIVER_MODULE_PATH := /system/wifi/ar6000.ko
+WIFI_DRIVER_MODULE_NAME := ar6000
+
+## RIL
+BOARD_USES_LEGACY_RIL := true
+BOARD_FORCE_RILD_AS_ROOT := true
+BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
+
+## UMS
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
+BOARD_UMS_LUNFILE := "/sys/devices/platform/usb_mass_storage/lun0/file"
+
+## Touchscreen
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+## Bootanimation
+TARGET_BOOTANIMATION_USE_RGB565 := true
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+
+## Audio
+TARGET_PROVIDES_LIBAUDIO := true
+
+## Misc.
+TARGET_PROVIDES_LIBLIGHTS := true
+TARGET_NO_INITLOGO := true
+TARGET_DISABLE_ARM_PIE := true
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+
+## Recovery
+BOARD_HAS_DOWNLOAD_MODE := true
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 229938816 # cannot build with default 219938816
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 190054400
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_KERNEL_CMDLINE := 
+BOARD_BML_BOOT := "/dev/block/bml8"
+BOARD_BML_RECOVERY := "/dev/block/bml9"
+BOARD_RECOVERY_HANDLES_MOUNT := true
---
+++ common.mk
@@ -0,0 +1,136 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## GPS configuration
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+DEVICE_PACKAGE_OVERLAYS += device/samsung/msm7x27-common/overlay
+
+## Media
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libmm-omxcore \
+    libstagefrighthw 
+
+## Display
+PRODUCT_PACKAGES += \
+    gralloc.msm7x27 \
+    hwcomposer.msm7x27 \
+    copybit.msm7x27 \
+    libQcomUI \
+    libgenlock \
+    liboverlay \
+    libtilerenderer 
+
+## Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    libaudioutils
+
+## Camera
+PRODUCT_PACKAGES += \
+    libcamera \
+    LegacyCamera 
+
+## GPS
+PRODUCT_PACKAGES += \
+    librpc 
+  
+## Other
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    brcm_patchram_plus \
+    bdaddr_read \
+    setup_fs  
+
+## Vold config
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/prebuilt/etc/vold.fstab:system/etc/vold.fstab
+
+## Hardware properties 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/base/data/etc/platform.xml:system/etc/permissions/platform.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+
+## Ramdisk
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/ramdisk/lib/modules/fsr.ko:root/lib/modules/fsr.ko \
+    device/samsung/msm7x27-common/ramdisk/lib/modules/fsr_stl.ko:root/lib/modules/fsr_stl.ko \
+    device/samsung/msm7x27-common/ramdisk/lib/modules/sec_param.ko:root/lib/modules/sec_param.ko
+
+## Prebuilt init.d scripts
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/prebuilt/etc/init.d/01bt:system/etc/init.d/01bt 
+
+## Wi-Fi & networking
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
+    device/samsung/msm7x27-common/prebuilt/etc/wifi/hostapd.conf:system/etc/wifi/hostapd.conf \
+    device/samsung/msm7x27-common/prebuilt/etc/wifi/dhcpcd.conf:system/etc/wifi/dhcpcd.conf \
+    device/samsung/msm7x27-common/prebuilt/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
+    device/samsung/msm7x27-common/prebuilt/bin/get_macaddrs:system/bin/get_macaddrs
+
+## Media
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
+    device/samsung/msm7x27-common/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
+    device/samsung/msm7x27-common/prebuilt/etc/audio_effects.conf:system/etc/audio_effects.conf \
+    device/samsung/msm7x27-common/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf \
+    device/samsung/msm7x27-common/prebuilt/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    device/samsung/msm7x27-common/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml \
+    device/samsung/msm7x27-common/prebuilt/etc/audio_policy.conf:system/etc/audio_policy.conf \
+
+## Keymap
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/sec_jack.kl:system/usr/keylayout/sec_jack.kl \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/Generic.kl:system/usr/keylayout/Generic.kl \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/sec_key.kl:system/usr/keylayout/sec_key.kl 
+
+## Keychar
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/qwerty.kcm:system/usr/keylayout/qwerty.kcm \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/qwerty2.kcm:system/usr/keylayout/qwerty2.kcm \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/Virtual.kcm:system/usr/keylayout/Virtual.kcm \
+    device/samsung/msm7x27-common/prebuilt/usr/keylayout/Generic.kcm:system/usr/keylayout/Generic.kcm
+
+## Touchscreen
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/prebuilt/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
+
+## ICS GPS blob
+PRODUCT_COPY_FILES += \
+    device/samsung/msm7x27-common/prebuilt/lib/hw/gps.msm7x27.so:system/lib/hw/gps.msm7x27.so
+
+# LDPI assets
+PRODUCT_LOCALES += en
+PRODUCT_AAPT_CONFIG := normal ldpi mdpi
+
+# Build.prop overrides
+PRODUCT_PROPERTY_OVERRIDES += \
+    hwui.render_dirty_regions=false \
+    pm.sleep_mode=1
