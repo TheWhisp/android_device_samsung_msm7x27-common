004eab130f2c263c295cf24b0a9dfc4322e6fe2e
Dāvis Mālnieks <daavis.90@gmail.com>
2012-09-29 10:14:21 +0300

    common: update audio and media

--- libaudio/AudioPolicyManager.h
+++ libaudio/AudioPolicyManager.h
@@ -42,8 +42,10 @@ protected:
         // true is current platform supports suplication of notifications and ringtones over A2DP output
         virtual bool a2dpUsedForSonification() const { return true; }
 #endif
-        // check that volume change is permitted, compute and send new volume to audio hardware
+
+// check that volume change is permitted, compute and send new volume to audio hardware
         status_t checkAndSetVolume(int stream, int index, audio_io_handle_t output, audio_devices_t device, int delayMs = 0, bool force = false);
+
 
 };
 };
