a15336d5c27f96ed27f8376aae8657454e917da3
Dāvis Mālnieks <daavis.90@gmail.com>
2012-10-14 17:38:55 +0300

    use prebuilt gps.conf

--- common.mk
+++ common.mk
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-## GPS configuration
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
-
 ## Media
 PRODUCT_PACKAGES += \
     libOmxCore \
