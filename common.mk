5a345d7d2dc3d01e43a0f6f63426232836f0ca48
erikcas <erikcas1972@gmail.com>
2012-10-25 16:36:39 +0200

    Add compcache (TheWhisp)

--- common.mk
+++ common.mk
@@ -84,7 +84,8 @@ PRODUCT_COPY_FILES += \
 
 ## Prebuilt init.d scripts
 PRODUCT_COPY_FILES += \
-    device/samsung/msm7x27-common/prebuilt/etc/init.d/01bt:system/etc/init.d/01bt  
+    device/samsung/msm7x27-common/prebuilt/etc/init.d/01bt:system/etc/init.d/01bt \
+    device/samsung/msm7x27-common/prebuilt/etc/init.d/02compcache:system/etc/init.d/02compcache 
 
 ## Wi-Fi & networking
 PRODUCT_COPY_FILES += \
